1f40c507bfcc439fcbd827c1f9ca4c9b8119bae5
Abhay Rana <capt.n3m0@gmail.com>
2014-07-14 08:19:42 +0530

    Refactor EvernoteScubber to its own class
    - Convert dtd and scrubber to class variables
    - Check Tidy output before passing it ahead
    -

---
+++ lib/evernote_scrubber.rb
@@ -0,0 +1,36 @@
+class EvernoteScrubber < Loofah::Scrubber
+  @@dtd = YAML.load_file Rails.root.join('public','dtd','enml2.yml')
+
+  def initialize
+    @direction = :bottom_up
+
+  end
+
+  def scrub(node)
+    # Remove all disallowed tags
+    if @@dtd['disallowed_tags'].include? node.name
+      node.remove
+      Loofah::Scrubber::STOP # don't bother with the rest of the subtree
+    end
+
+    # Remove all disallowed attributes
+    node.attribute_nodes.each do |attr_node|
+      # Get the attribute name if we have a namespace
+      attr_name = if attr_node.namespace
+        "#{attr_node.namespace.prefix}:#{attr_node.node_name}"
+      else
+        attr_node.node_name
+      end
+      if @@dtd['disallowed_attributes'].include? attr_name
+        attr_node.remove
+      end
+    end
+
+    # This is lifted from the strip scrubber in
+    # Loofah. It replaces the node with its contents
+    unless @@dtd['allowed_tags'].include? node.name
+      node.before node.children
+      node.remove
+    end
+  end
+end
--- lib/evernote_util.rb
+++ lib/evernote_util.rb
@@ -1,7 +1,9 @@
 require 'evernote-thrift'
 require 'loofah'
-
+require 'xml'
 class EvernoteUtil < Evernote::EDAM::NoteStore::NoteStore::Client
+  @@scrubber = EvernoteScrubber.new
+  @@dtd = XML::Dtd.new File.read Rails.root.join('public', 'dtd','enml2.dtd')
   def self.textToENML(text)
     # TODO: Convert to heredoc
     body  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
@@ -47,7 +49,7 @@ class EvernoteUtil < Evernote::EDAM::NoteStore::NoteStore::Client
     note.tagNames = tags.map &:squish # Tag names may not begin or end with a space. 
     note.notebookGuid = notebook_guid if notebook_guid
     note.attributes = self.create_note_attribute url, source
-    self.class.validate_enml note.content
+    raise 'ENML Validation failed' unless self.class.validate_enml note.content
     
     begin
       self.createNote @token, note
@@ -88,8 +90,15 @@ class EvernoteUtil < Evernote::EDAM::NoteStore::NoteStore::Client
     tidy.options.char_encoding = 'utf8'
     tidy.options.output_encoding = 'utf8'
     tidy.options.indent = 1
+    tidy.options.show_warnings = true
+    tidy.options.new_blocklevel_tags = 'article, heading'
+    doc = tidy.clean
+    if doc.nil?
+      puts tidy.errors
+      raise 'Invalid HTML'
+    end
     # We force encode coz the above tidy options don't seem to work
-    doc = tidy.clean.force_encoding('UTF-8')
+    doc.force_encoding('UTF-8')
     # tidy.clean returns html with doctype and everything
     # So we pick the body tag's inner html
     return doc[(doc.index("<body>")+6)..(doc.index("</body>")-1)]
@@ -107,44 +116,13 @@ class EvernoteUtil < Evernote::EDAM::NoteStore::NoteStore::Client
   # Takes in invalid enml and
   # Removes all unsafe attributes/tags
   def self.sanitize(enml)
-    dtd = YAML.load_file Rails.root.join('public','dtd','enml2.yml')
-    scrubber = Loofah::Scrubber.new do |node|
-      # Remove all disallowed tags
-      if dtd['disallowed_tags'].include? node.name
-        # We strip the html tag, keeping its children
-        # The HTML tag is inserted by Tidy
-        
-        node.remove
-        Loofah::Scrubber::STOP # don't bother with the rest of the subtree
-      end
-
-      # Remove all disallowed attributes
-      node.attribute_nodes.each do |attr_node|
-        # Get the attribute name if we have a namespace
-        attr_name = if attr_node.namespace
-          "#{attr_node.namespace.prefix}:#{attr_node.node_name}"
-        else
-          attr_node.node_name
-        end
-        if dtd['disallowed_attributes'].include? attr_name
-          attr_node.remove
-        end
-      end
-    end
-    Loofah.fragment(enml).scrub!(scrubber).to_s
+    Loofah.fragment(enml).scrub!(@@scrubber).to_s
   end
+
 
   def self.validate_enml(enml)
-    require 'xml'
-    dtd = XML::Dtd.new File.read Rails.root.join('public', 'dtd','enml2.dtd')
     enml_document = XML::Document.string enml
-    enml_document.validate dtd
+    enml_document.validate @@dtd
   end
 
-  def self.save_enml(filename, enml)
-    tmp_file = "#{Rails.root}/tmp/#{filename}"
-    File.open(tmp_file, 'wb') do |f|
-      f.write enml
-    end
-  end
 end
