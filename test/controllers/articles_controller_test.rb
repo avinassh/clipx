db949074ecfa74b81ee20f4190e8e6f88c8e7a82
Abhay Rana <capt.n3m0@gmail.com>
2014-08-01 22:51:07 +0530

    SEARCH: Tests for proper searches.

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -76,4 +76,17 @@ class ArticlesControllerTest < ActionController::TestCase
     assert_equal @article.user_id, users(:default).id
     assert_equal @article.status, 'fetched'
   end
+
+  test 'should search articles' do
+    Article.all.reindex
+    get :index, { :format => :json, :q => "Sample Title" }
+    assert_response :success
+    articles = JSON.parse(@response.body)
+    assert_equal "Sample Title", articles[0]['title']
+
+    get :index, { :format => :json, :q => "GDOFDGSFDHSJF" }
+    assert_response :success
+    articles = JSON.parse(@response.body)
+    assert_equal 0, articles.length
+  end
 end
